4adbdf3776c60738a2bcfd76fc42517b9d3ba861
SAKTHIVEL MURUGASAMY <sakthivel.murugasamy@conversight.ai>
2022-04-18 00:11:54 +0530

    Comment update

--- src/features/booking/BookingListAPI.ts
+++ src/features/booking/BookingListAPI.ts
@@ -1,6 +1,10 @@
 import { LIST_BOOKINGS, LIST_ROOMS, LIST_SLOTS } from '../../app/config';
 import { Booking } from './../../app/types/index';
 
+/**
+ * Fetch initial data such as rooms and slots
+ * @returns [rooms[], slots[]]
+ */
 export async function fetchInitData(): Promise<any> {
   return Promise.all([
     fetch(LIST_ROOMS).then(resp => resp.json()),
@@ -8,7 +12,10 @@ export async function fetchInitData(): Promise<any> {
   ])
 }
 
-
+/**
+ * Fetch bookings data
+ * @returns bookings[]
+ */
 export async function fetchBookings(): Promise<Booking[]> {
   return fetch(LIST_BOOKINGS)
     .then(response => response.json())
